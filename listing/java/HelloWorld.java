e2729dacc88ca36c2e28cd8d74e673899ccab0f4
srcalex <srcalex@web.de>
2015-11-12 20:51:58 +0100

    Finish section one so far

---
+++ listing/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class {
+  /**
+   *
+   * This is a multi line comment.
+   *
+   */
+  public static void main(String[] args) {
+    // This is a single line comment
+    System.out.println("Hello World");
+  }
+}
