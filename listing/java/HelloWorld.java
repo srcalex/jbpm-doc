ff558dacd128be439b5216fca948d33d0e60f18c
srcalex <srcalex@web.de>
2015-11-12 22:18:43 +0100

    Refactor listing example

--- listing/java/HelloWorld.java
+++ listing/java/HelloWorld.java
@@ -1,11 +1,6 @@
-public class {
-  /**
-   *
-   * This is a multi line comment.
-   *
-   */
+public class HelloWorld {
+
   public static void main(String[] args) {
-    // This is a single line comment
     System.out.println("Hello World");
   }
 }
